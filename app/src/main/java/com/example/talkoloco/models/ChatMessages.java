ba8cd3da13c7c9bf61bc575067c2109c52ae4248
israelfernandez7 <fernandezisrael2000@gmail.com>
2024-11-22 23:53:25 -0800

    Created informational objects for the chat.

---
+++ app/src/main/java/com/example/talkoloco/models/ChatMessages.java
@@ -0,0 +1,12 @@
+package com.example.talkoloco.models;
+
+import java.util.Date;
+
+public class ChatMessages {
+
+    // Informational objects of the chat
+    public String senderId, receiverID, message, dateTime;
+
+    // Object for the date
+    public Date dateObject;
+}
