94cf52f6c39c5d22429ea977e46c68968cc58d1e
israelfernandez7 <fernandezisrael2000@gmail.com>
2024-11-23 02:48:47 -0800

    Added and modified the appropriate constants.

--- app/src/main/java/com/example/talkoloco/utils/Constants.java
+++ app/src/main/java/com/example/talkoloco/utils/Constants.java
@@ -1,6 +1,7 @@
 package com.example.talkoloco.utils;
 
 public class Constants {
+
     // firebase collections
     public static final String KEY_COLLECTION_USERS = "users";
     public static final String KEY_COLLECTION_CHAT = "chats";
@@ -16,6 +17,8 @@ public class Constants {
     public static final String KEY_LAST_LOGIN = "lastLoginAt";
 
     // chat fields
+
+    public static final String KEY_USER = "user";
     public static final String KEY_SENDER_ID = "senderId";
     public static final String KEY_RECEIVER_ID = "receiverId";
     public static final String KEY_MESSAGE = "message";
@@ -24,5 +27,10 @@ public class Constants {
     // shared preferences
     public static final String KEY_PREFERENCE_NAME = "talkolocoPrefs";
     public static final String KEY_IS_SIGNED_IN = "isSignedIn";
+
+
+
+
+
 
 }
